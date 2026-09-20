f5c8a43dbee2a7122f1beb09aa4b4fa6e5e59773
Chickencrush <syandi.chickencrush@gmail.com>
2026-09-20 21:41:18 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ccqc-v3.2.2';
+const CACHE='ccqc-v3.2.3';
 const ASSETS=['./','./index.html','./config.js','./api-bridge.js','./manifest.webmanifest','./offline.html','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
