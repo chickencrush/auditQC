46adcd9da792f986797a3574e541c0d884e9f706
Chickencrush <syandi.chickencrush@gmail.com>
2026-09-20 14:08:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* Paste URL deployment Google Apps Script /exec Anda di sini. */
 window.CCQC_CONFIG = {
-  APP_URL: 'PASTE_GOOGLE_APPS_SCRIPT_EXEC_URL_HERE',
+  APP_URL: 'https://script.google.com/macros/s/AKfycbyckvaguEsK7e1cReyv54dfPsdkfFvSRl6XebtcjhuTIuKKqV6k4XVZvRfLbTI9iys/exec',
   APP_VERSION: '3.0.1',
   APP_NAME: 'Chicken Crush QC'
 };
